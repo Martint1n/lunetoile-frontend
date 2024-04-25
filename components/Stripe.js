b24067d2d00cd01e2462ebd753e8b9d4cc0db19b
guilbert <guilbertmartin75@gmail.com>
2024-04-25 23:31:05 +0200

    change Stripe.js

--- components/Stripe.js
+++ components/Stripe.js
@@ -1,9 +1,30 @@
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
+import { MdCardMembership } from 'react-icons/md';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_51P9EqSEsrCdUsLMuJUcvfBoUVVmVEIPFgBus8RRnVkTCZb9ZqeSdHnnjQdxiTpyXT4W4qG4gAZCDQesaNcyYMdfj00tFOAC5Am''pk_test_51P9EqSEsrCdUsLMuJU...aNcyYMdfj00tFOAC5Am');
+const preorder = {
+    product: card,
+    price: 29.99,
+}
+
+
+const makePayment = async () => {
+    const stripe = await loadStripe('pk_test_51P9EqSEsrCdUsLMuJUcvfBoUVVmVEIPFgBus8RRnVkTCZb9ZqeSdHnnjQdxiTpyXT4W4qG4gAZCDQesaNcyYMdfj00tFOAC5Am');
+
+const response = await fetch(`${apiURL}/create-checkout-session`, {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ product: preorder })
+
+})
+
+const session = await response.json()
+
+const result = stripe.redirectToCheckout;
+
+}
 
 export default function Stripe() {
     const options = {
--- pages/[preorder].js
+++ pages/[preorder].js
@@ -1,8 +1,14 @@
 import Preorder from '../components/Preorder';
+import Stripe from '../components/Stripe'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function Index() {
-    return <Preorder />;
+    return (
+        <>
+        <Preorder />
+        <Stripe />
+        </>
+    )
 }
 
 export default Index;
