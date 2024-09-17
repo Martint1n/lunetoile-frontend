1ccf1f8561d709e781f1c9390366c6758c4754b2
guilbert <guilbertmartin75@gmail.com>
2024-09-17 20:26:13 +0200

    add webpack to nextconfig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
@@ -5,6 +7,16 @@ const nextConfig = {
   trailingSlash: false,
   images: {
     unoptimized: false,
+  },
+  webpack: (config) => {
+    // Ajoutez l'alias pour les modules afin de pointer directement vers 'main.js'
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@react-stately/utils': path.resolve(__dirname, 'node_modules/@react-stately/utils/dist/main.js'),
+    };
+    
+    // Assurez-vous de retourner le config modifié
+    return config;
   },
 };
 
