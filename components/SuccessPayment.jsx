c1169f87696658359bd28f27d1f2c08267f99a18
guilbert <guilbertmartin75@gmail.com>
2024-09-18 21:56:57 +0200

    remove redux from succeesPage component

--- components/SuccessPayment.jsx
+++ components/SuccessPayment.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 
 function SuccessPayment() {
-    const preorderInfos = useSelector((state) => state.preorderInfos.value);
+    // const preorderInfos = useSelector((state) => state.preorderInfos.value);
 
-    useEffect(() => { //à tester sans le useEffect pour voir s'il ajoute 1 ou 2 documents à la database
-        console.log('preorderInfo :', preorderInfos)
-        console.log(preorderInfos.artist)
+    // useEffect(() => { //à tester sans le useEffect pour voir s'il ajoute 1 ou 2 documents à la database
+    //     console.log('preorderInfo :', preorderInfos)
+    //     console.log(preorderInfos.artist)
 
-        fetch(`http://localhost:3000/preorder/${preorderInfos.artist}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(preorderInfos),
-        })
-        .then((response) => {
-            response.json()
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    }, [])
+    //     fetch(`http://localhost:3000/preorder/${preorderInfos.artist}`, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(preorderInfos),
+    //     })
+    //     .then((response) => {
+    //         response.json()
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
+    // }, [])
 
-    window.onbeforeunload = function() {
-        localStorage.clear();
-    }
+    // window.onbeforeunload = function() {
+    //     localStorage.clear();
+    // }
 
     return (
         <div>successpayment</div>
