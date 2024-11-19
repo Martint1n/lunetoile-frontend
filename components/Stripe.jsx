7aa6ba110f3a48b5fc8a9081ed9857e046998ac4
guilbert <guilbertmartin75@gmail.com>
2024-11-19 02:02:39 +0100

    change makePayment in Stripe into makeTest to test DB

--- components/Stripe.jsx
+++ components/Stripe.jsx
@@ -30,7 +30,7 @@ export default function Stripe({ firstName, lastName, country, phoneNumber, addr
   };
 
   const makeTest = async () => {
-    const response = await fetch('https://lunetoile-backend.vercel.app/testpayment/create-checkout-session', {
+    const response = await fetch('https://lunetoile-backend.vercel.app/testpayment/testdb', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({}),
@@ -41,7 +41,7 @@ export default function Stripe({ firstName, lastName, country, phoneNumber, addr
   };
 
   return (
-    <button onClick={makePayment} disabled={!stripePromise} className={`rounded-3xl w-full h-12`}>
+    <button onClick={makeTest} disabled={!stripePromise} className={`rounded-3xl w-full h-12`}>
       Précommander
     </button>
   );
