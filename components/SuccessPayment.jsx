fb776d83991e85a1a0947f85a7b8a2ac1a81a1c7
guilbert <guilbertmartin75@gmail.com>
2024-11-27 18:21:05 +0100

    remove method in get fetch

--- components/SuccessPayment.jsx
+++ components/SuccessPayment.jsx
@@ -26,10 +26,7 @@ function SuccessPayment() {
 
       try {
         console.log('sessionId_2', sessionId)
-        const response = await fetch(`https://lunetoile-backend.vercel.app/testpayment/sendmails?sessionId=${sessionId}`, {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' },
-        });
+        const response = await fetch(`https://lunetoile-backend.vercel.app/testpayment/sendmails?sessionId=${sessionId}`);
 
         const result = await response.json();
 
