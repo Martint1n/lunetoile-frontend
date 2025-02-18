3f74d5b9143e927e7b2d77f748e23eb6596895e2
Martint1n <guilbertmartin75@gmail.com>
2025-02-18 20:55:46 +0100

    change aartist to artistHome in ArtistPage func

--- pages/[artistHome].js
+++ pages/[artistHome].js
@@ -36,7 +36,7 @@ export async function getServerSideProps(context) {
   if (artistCache[cleanedArtist] && currentTime - artistCache[cleanedArtist].timestamp < CACHE_DURATION) {
     return {
       props: {
-        artist: cleanedArtist,
+        artistHome: cleanedArtist,
         isAllowed: artistCache[cleanedArtist].isAllowed,
       },
     };
@@ -59,14 +59,14 @@ export async function getServerSideProps(context) {
 
     return {
       props: {
-        artist: cleanedArtist,
+        artistHome: cleanedArtist,
         isAllowed,
       },
     };
   } catch (error) {
     return {
       props: {
-        artist: cleanedArtist,
+        artistHome: cleanedArtist,
         isAllowed: false,
       },
     };
