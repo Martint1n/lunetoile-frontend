e6877d71660f753e89d69cfb2675f992c92ec311
Maximilien Gilet <maximilien248@gmail.com>
2025-03-13 13:29:48 +0100

    fix: home page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,43 +1,49 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className='flex flex-col justify-end items-center py-6 lg:py-8'>
-      <Image 
-        src='/logoblack.svg' 
-        width={144} 
-        height={35} 
-        alt="Lunétoile Logo" 
-        className='mb-4 w-[120px] lg:w-[144px]'
+    <footer className="flex flex-col justify-end items-center py-6 lg:py-8">
+      <Image
+        src="/logoblack.svg"
+        width={144}
+        height={35}
+        alt="Lunétoile Logo"
+        className="mb-4 w-[120px] lg:w-[144px]"
       />
-      <div className='max-w-4xl px-6 lg:px-0'>
+      <div className="max-w-4xl px-6 lg:px-0">
         {/* Desktop view */}
-        <div className='hidden lg:flex body-s flex-wrap justify-center items-center gap-x-4'>
-          <Link href="/conditions-generales-de-vente" className='hover:text-gold'>
+        <div className="hidden lg:flex body-s flex-wrap justify-center items-center gap-x-4">
+          <Link
+            href="/conditions-generales-de-vente"
+            className="hover:text-gold"
+          >
             Conditions générales de vente
           </Link>
-          <span className='text-lightGrey'>•</span>
-          <Link href="/mentions-legales" className='hover:text-gold'>
+          <span className="text-lightGrey">•</span>
+          <Link href="/mentions-legales" className="hover:text-gold">
             Mentions légales
           </Link>
-          <span className='text-lightGrey'>•</span>
-          <Link href="/confidentialite" className='hover:text-gold'>
+          <span className="text-lightGrey">•</span>
+          <Link href="/confidentialite" className="hover:text-gold">
             Confidentialité
           </Link>
-          <span className='text-lightGrey'>•</span>
+          <span className="text-lightGrey">•</span>
           <span>Copyright Lunétoile {new Date().getFullYear()}</span>
         </div>
 
         {/* Mobile view */}
-        <div className='flex lg:hidden body-s flex-col text-center gap-y-1'>
-          <Link href="/conditions-generales-de-vente" className='hover:text-gold'>
-            conditions générales de vente
+        <div className="flex lg:hidden body-s flex-col text-center gap-y-1">
+          <Link
+            href="/conditions-generales-de-vente"
+            className="hover:text-gold"
+          >
+            Conditions générales de vente
           </Link>
-          <Link href="/mentions-legales" className='hover:text-gold'>
+          <Link href="/mentions-legales" className="hover:text-gold">
             Mentions légales
           </Link>
-          <Link href="/confidentialite" className='hover:text-gold'>
+          <Link href="/confidentialite" className="hover:text-gold">
             Confidentialité
           </Link>
           <span>Copyright Lunétoile {new Date().getFullYear()}</span>
@@ -47,4 +53,5 @@ function Footer() {
   );
 }
 
-export default Footer; 
+export default Footer;
+
