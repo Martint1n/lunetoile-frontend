eec9f8224ef75e06f9b34c538cb09ecc503a0630
Martint1n <guilbertmartin75@gmail.com>
2025-04-04 19:15:00 +0200

    update cgv, confidentialite and mentionslegales component

--- pages/cgv.js
+++ pages/cgv.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import cgv from '../components/cgv';
+import cgvTest from '../components/cgv'
 
 
 
 function cgvPage() {
 
     return (
-        <cgv />
+        <cgvTest />
     )
 }
 
