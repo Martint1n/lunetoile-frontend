259aa59d94dd8aa216fe10394a1e4126945d57b1
Martint1n <guilbertmartin75@gmail.com>
2025-04-05 02:53:08 +0200

    chaange favicon and google descritpion

--- pages/_app.js
+++ pages/_app.js
@@ -34,11 +34,13 @@ function App({ Component, pageProps }) {
       //   <PersistGate persistor={persistor}>
         <NextUIProvider>
           <Head>
-            <title>Next.js App</title>
+            <title>Lunetoile</title>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, orientation=portrait"/>
+            <link rel="icon" type="image/svg+xml" href="/miniature.svg" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, orientation=portrait "/>
+            <meta name="description" content="82 artistes - Un objet unique. Précommande disponible jusqu’au 04 mai"/>
           </Head>
           {getLayout(<Component {...pageProps} />)}
           </NextUIProvider>
