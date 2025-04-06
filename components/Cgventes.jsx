9d626b95f7423e34b22280320fff7f10fc9857aa
Martint1n <guilbertmartin75@gmail.com>
2025-04-06 12:07:38 +0200

    add  router to cgv

--- components/Cgventes.jsx
+++ components/Cgventes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Cgventes = () => {
+  const router = useRouter();
   return (
     <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg space-y-6">
       <div className="sticky top-0 bg-white flex flex-col justify-end items-center">
-        <Link href={`/${artist}`}>
+        <Link href={router.back()}>
           <Image
             src="/logoblack.svg"
             width={144}
